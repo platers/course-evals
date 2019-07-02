39470a1f401d27afa7da3d4e83ddca2add706d2f
Victor <platers81@gmail.com>
2019-07-01 22:00:12 -0400

    idk

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
-d3.csv("/cleandata.csv").then(function(data) {
+
+
+d3.csv("https://raw.githubusercontent.com/platers/course-evals/master/cleandata.csv", function (data) {
+    var columns = ['dept', 'division', 'course', 'last_name', 'first_name', 'term', 'year', 'item1', 'item2', 'item3', 'item4', 'item5', 'item6', 'enthusiasm', 'workload', 'recommend', 'invited', 'respondents', 'code', 'name']
     console.log(data[0]);
-  });
-  
+})
